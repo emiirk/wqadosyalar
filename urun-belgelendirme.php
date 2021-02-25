84e83c86fbe60b448f4ae068e3bf3711e16ded54
Ramazan Sert <rramazansert@gmail.com>
2021-02-25 09:39:49 +0300

    wqa dosyaları atıldı

---
+++ urun-belgelendirme.php
@@ -0,0 +1,76 @@
+<?php require_once 'header.php';?>
+
+<style>
+  .section-heading p, .service-block p{
+    color:#383838 !important;
+  }
+</style>
+<!-- Page Title START -->
+<div class="page-title-section" style="background-image: url(img/slider/slide11.jpg);">
+  <div class="container">
+    <h1>Ürün Belgelendirme</h1>
+    <ul>
+      <li><a href="index.html">Anasayfa</a></li>
+      <li><a href="about-2.html">Ürün Belgelendirme</a></li>
+    </ul>
+  </div>
+</div>
+<!-- Page Title END -->   
+<div class="section-block">
+  <div class="container">
+    <div class="section-heading center-holder">
+      <h3>Ürün Belgelendirme Hizmetlerimiz</h3>
+      <div class="section-heading-line"></div>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It</p>
+    </div>
+    <div class="row mt-50">
+      <div class="col-md-4 col-sm-8 col-12">
+        <div class="service-block">
+          <img src="img/icon/kalite-yonetim-icon.png" alt="service">
+          <div class="clearfix">
+            <div class="service-block-number">
+              <h5>01</h5>
+            </div>
+            <div class="service-block-title">
+              <h4><a href="#">ISO 9001 Kalite Yönetim Sistemi</a></h4>
+            </div>
+          </div>
+          <p>International Organization For Standardization cümlesinin kısaltması olan ISO 9001: 2015 belgesi, kalite yönetim standartlarının oluşturulması ve yürütülmesini kapsamaktadır. </p>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-8 col-12">
+        <div class="service-block">
+          <img src="img/icon/cevre-yonetim-icon.png" alt="service">
+          <div class="clearfix">
+            <div class="service-block-number">
+              <h5>02</h5>
+            </div>
+            <div class="service-block-title">
+              <h4><a href="#">ISO 14001 Çevre Yönetim Sistemi</a></h4>
+            </div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-8 col-12">
+        <div class="service-block">
+          <img src="img/icon/gida-guvenligi-icon.png" alt="service">
+          <div class="clearfix">
+            <div class="service-block-number">
+              <h5>03</h5>
+            </div>
+            <div class="service-block-title">
+              <h4><a href="#">22000 Gıda Güvenliği Yönetim Sistemi</a></h4>
+            </div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+<?php require_once 'footer-top.php'; ?>
+<?php require_once 'footer.php'; ?>
